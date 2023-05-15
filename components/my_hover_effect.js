bcbf65eea21963ddd3c8871033f96ec04d323fcb
Isabela Nery <isabelanery.r@gmail.com>
2023-05-15 20:25:39 -0300

    feat: fix animation full screen

--- components/my_hover_effect.js
+++ components/my_hover_effect.js
@@ -3,11 +3,11 @@ import styles from '../styles/my_hover_effect.module.css';
 export default function MyHoverEffect() {
     return (
       <div class={styles.body}>
-        <p class={styles.p}>Haki</p>
+        <p class={styles.p}>oi gatinho</p>
 
         <div class={styles.card}>
           {
-            [...Array(480).keys()].map((i) => (
+            [...Array(312).keys()].map((i) => (
               <div class={styles.snow}></div>
             ))
           }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import MyLayout from "../components/my_layout";
 import utilStyles from "../styles/utils.module.css";
 import { getOnePieceData } from "../lib/get_one_piece_data";
+import MyHoverEffect from "../components/my_hover_effect";
 
 export async function getStaticProps() {
   const allHakis = await getOnePieceData('hakis');
@@ -12,20 +13,8 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home ({ allHakis }) { 
+export default function Home ({ allHakis }) {
   return (
-    <MyLayout home>
-      <section className={utilStyles.headingMd}>
-        <Link href="battles/first-battle">
-          <p>Fight Log</p>
-        </Link>
-        </section>
-        
-        <h2>
-          <Link href="hakis">
-            <p>Hakis</p>
-          </Link>
-        </h2>
-    </MyLayout>
+    <MyHoverEffect />
   );
 }
